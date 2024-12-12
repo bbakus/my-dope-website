2c9a534301f5f098cdeb1c8d2cccaaf629227c7a
Brandon Bakus <brandonbakus@gmail.com>
2024-12-12 14:07:17 -0800

    Adjusted header contents

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "A basic intro for beginners";
 
 document.querySelector("body").appendChild(h2);
