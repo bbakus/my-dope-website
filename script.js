1c621ffd2847cd9f49abc07be0f24b7a68351b21
Brandon Bakus <brandonbakus@gmail.com>
2024-12-12 16:33:35 -0800

    FINALLY FUCKING CENTERED THE PARAGRAPH TEXT OH MY FUCKING GOD

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
 const h2 = document.createElement("h2");
-h2.textContent = "A basic intro for beginners";
+h2.textContent = "A basic introduction for beginners";
 
 document.querySelector("body").appendChild(h2);
